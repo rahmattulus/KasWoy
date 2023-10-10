8fbfd912190a19a3ffa16d52e22ef68df353902a
rahmattulus <rahmattuluz01@gmail.com>
2023-10-10 21:40:02 +0700

    100%??

--- admin/js/demo/chart-pie-demo.js
+++ admin/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Pemasukan", "Pengluaran", "Saldo"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [45, 10, 45],
+      backgroundColor: ['#4e73df', '#e74a3b', ' #1cc88a'],
+      hoverBackgroundColor: ['#2e59d9', '#be2617', '#13855c'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
